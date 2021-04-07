2e057e189a52b9cb05182da340b3a7886432e5bb
priyanka nigam <priyanka.nigam_cs18@gla.ac.in>
2021-04-08 00:13:51 +0530

    quiz

---
+++ quizapp/src/components/QuestionBox.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+// import React from "react";
+const QuestionBox = ({ question, answers, selected }) => {
+  const [answer, setAnswer] = useState(answers);
+  return (
+    <div className="questionBox">
+      <div className="question">{question}</div>
+
+      {answer.map((text, index) => (
+        <button
+          key={index}
+          className="answerBtn"
+          onClick={() => {
+            setAnswer([text]);
+            selected({ text });
+          }}
+        >
+          {text}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default QuestionBox;
